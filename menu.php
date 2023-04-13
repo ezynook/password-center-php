84eb09479d98f90c0d9f5caba709f30f05f232a8
พสิษฐ์ ยอดสร้อย <ezynook@gmail.com>
2023-04-13 12:30:39 +0700

    Update Code Automate at: 2023-04-13 12:30:39

--- menu.php
+++ menu.php
@@ -4,8 +4,10 @@
             <h3><a href="#">Camera Generate Password</a></h3>
             <ul>
                 <li class="<?php if ($_GET['menu'] == 'table-search'){echo 'active';}else{ echo '';} ?>"><a href="table-search.php?menu=table-search">ค้นหา</a></li>
-                <li class="<?php if ($_GET['menu'] == 'encode'){echo 'active';}else{ echo '';} ?>"><a href="encode.php?menu=encode">เข้ารหัส</a></li>
-                <li class="<?php if ($_GET['menu'] == 'decode'){echo 'active';}else{ echo '';} ?>"><a href="decode.php?menu=decode">ถอดรหัส</a></li>
+                <?php if (isset($_SESSION['Username'])){ ?>
+                    <li class="<?php if ($_GET['menu'] == 'encode'){echo 'active';}else{ echo '';} ?>"><a href="encode.php?menu=encode">เข้ารหัส</a></li>
+                    <li class="<?php if ($_GET['menu'] == 'decode'){echo 'active';}else{ echo '';} ?>"><a href="decode.php?menu=decode">ถอดรหัส</a></li>
+                <?php } ?>
             </ul>
             <ul class="nav secondary-nav">
                 <li class="menu">
