e9906af5b4b9a274ff67fe8ff4da6e67d22a1eaa
พสิษฐ์ ยอดสร้อย <ezynook@gmail.com>
2023-04-14 19:35:02 +0700

    Update Code Automate at: 2023-04-14 19:35:02

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-    header("location: table-search.php?menu=table-search");
+    if (!isset($_SESSION)){
+        session_start();
+    }
+    $check_auth = $_COOKIE['Username'];
+
+    if (isset($check_auth)){
+        $_SESSION['Username'] = $check_auth;
+        header("location: table-search.php?menu=table-search");
+    }else{
+        header('location: ../Auth.php');
+    }
 ?>
