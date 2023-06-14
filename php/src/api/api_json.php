6dd41789a5973708d3fbbdea63e7425118da4dfe
Pasit Y <ezynook@gmail.com>
2023-06-14 10:59:48 +0700

    Update Code at: 2023-06-14 10:59:48

---
+++ php/src/api/api_json.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+require '../include/db.php';
+
+if (isset($_GET['customer']) && $_GET['customer'] != ""){
+    $customer = $_GET['customer'];
+    if (isset($_GET['site'])){
+        $site = $_GET['site'];
+        $filter = "WHERE customer LIKE '%{$customer}%' AND site LIKE '%{$site}%'";
+    }else{
+        $filter = "WHERE customer LIKE '%{$customer}%'";
+    }
+}else{
+    $filter = "";
+}
+$sql = "SELECT * FROM tbl_password {$filter}";
+$query = mysqli_query($conn, $sql);
+
+$group_items = array();
+
+  foreach($query as $items){
+      $group_items[$items['customer']][] = $items;
+  }
+  $res = json_encode($group_items);
+  echo $res;
+?>
